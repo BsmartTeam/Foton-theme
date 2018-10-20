c0f115ccaf7c001fe2e7461a85bbd5d1da44d1dd
Dan Zubco <danzubco@gmail.com>
2018-10-20 14:58:51 +0300

    improve piechart: on viewport start animation

--- sections/piechart/bst_piechart.js
+++ sections/piechart/bst_piechart.js
@@ -18,13 +18,15 @@ class PieChartCircle {
         this.transition = transition || 1000;
         this.rate = 1000 / 60;
         this.stop_loop = this.transition / this.rate;
+        this.rendered = false;
+        this.triggerDistance = 100;
 
         // Elemente
         this.progress_counter = $('<span>').addClass('bst-pc-percent');
         this.canvas = $('<canvas>').attr('width', '190').attr('height', '190');
     }
 
-    draw() {
+    render() {
         // Initializare
         $(this.piechart_circle)
             .css('width', this.circle_radius * 2)
@@ -47,46 +49,92 @@ class PieChartCircle {
         this.progress_counter.text(this.percent);
 
         if (this.animated) {
-            // Animated Progress Arc
             var self = this;
-            self.timer_rate = Math.floor(self.transition / self.percent);
-            self.percent_count = 0;
-            self.timer = setInterval(function () {
-                self.ctx.clearRect(0, 0, self.canvas.width, self.canvas.height);
-                self.ctx.beginPath();
-                self.ctx.strokeStyle = self.color_circle;
-                self.ctx.lineWidth = self.line_width_circle;
-                self.ctx.arc(self.circle_radius, self.circle_radius, (self.circle_radius - (self.line_width_progress / 2)), 2 * Math.PI, false);
-                self.ctx.closePath();
-                self.ctx.stroke();
-                self.ctx.beginPath();
-                self.ctx.strokeStyle = self.bar_color;
-                self.ctx.lineWidth = self.line_width_progress;
-                self.ctx.lineCap = 'round';
-                self.ctx.arc(self.circle_radius, self.circle_radius, (self.circle_radius - (self.line_width_progress / 2)), 1.5 * Math.PI, (-1.5 + (self.progress_start * 0.02)* Math.PI));
-                self.ctx.stroke();
+            // https://medium.com/talk-like/detecting-if-an-element-is-in-the-viewport-jquery-a6a4405a3ea2
+            // http://stackoverflow.com/a/33979503/4374566
+            // https://codepen.io/BoyWithSilverWings/pen/MJgQqR
+            // Verifica daca elementul este in viewport si initializeaza functia de desenare
+            $(window).scroll(function () {
+                var top_of_element = $(self.piechart_circle).offset().top;
+                var bottom_of_element = $(self.piechart_circle).offset().top + $(self.piechart_circle).outerHeight();
+                var top_of_screen = $(window).scrollTop();
+                var bottom_of_screen = $(window).scrollTop() + window.innerHeight;
 
-                self.progress_start++;
-                self.percent_count++;
-
-                self.progress_counter.text(self.percent_count);
-
-                if (self.progress_start >= self.percent) {
-                    clearInterval(self.timer);
+                if ((bottom_of_screen > (top_of_element + self.triggerDistance)) && (top_of_screen < (bottom_of_element - self.triggerDistance))) {
+                    // Executa cand elementul apare in vieport
+                    if (!self.rendered) {
+                        self.drawAnimated();
+                    }
+                    self.rendered = true;
+                } else {
+                    // Executa daca elementul nu este vizibil
                 }
-            }, this.timer_rate);
+            });
+
+            // La incarcarea documentului verifica daca elementul se afla in viewport
+            $(window).ready(function () {
+                var top_of_element = $(self.piechart_circle).offset().top;
+                var bottom_of_element = $(self.piechart_circle).offset().top + $(self.piechart_circle).outerHeight();
+                var top_of_screen = $(window).scrollTop();
+                var bottom_of_screen = $(window).scrollTop() + window.innerHeight;
+
+                if ((bottom_of_screen > (top_of_element + self.triggerDistance)) && (top_of_screen < (bottom_of_element - self.triggerDistance))) {
+                    // Executa cand elementul apare in vieport
+                    if (!self.rendered) {
+                        self.drawAnimated();
+                    }
+                    self.rendered = true;
+                } else {
+                    // Executa daca elementul nu este vizibil
+                }
+            });
 
         } else {
-            // Static Progress Arc
-            this.ctx.beginPath();
-            this.ctx.strokeStyle = this.bar_color;
-            this.ctx.lineWidth = this.line_width_progress;
-            this.ctx.lineCap = 'round';
-            this.ctx.arc(this.circle_radius, this.circle_radius, (this.circle_radius - (this.line_width_progress / 2)), 1.5 * Math.PI, (1.5 + (this.percent * 0.02)) * Math.PI);
-            this.ctx.stroke();
-
-            this.progress_counter.text(this.percent);
+            this.drawStatic();
         }
+    }
+
+    drawAnimated() {
+        // Animated Progress Arc
+        var self = this;
+        self.timer_rate = Math.floor(self.transition / self.percent);
+        self.percent_count = 0;
+        self.timer = setInterval(function () {
+            self.ctx.clearRect(0, 0, self.canvas.width, self.canvas.height);
+            self.ctx.beginPath();
+            self.ctx.strokeStyle = self.color_circle;
+            self.ctx.lineWidth = self.line_width_circle;
+            self.ctx.arc(self.circle_radius, self.circle_radius, (self.circle_radius - (self.line_width_progress / 2)), 2 * Math.PI, false);
+            self.ctx.closePath();
+            self.ctx.stroke();
+            self.ctx.beginPath();
+            self.ctx.strokeStyle = self.bar_color;
+            self.ctx.lineWidth = self.line_width_progress;
+            self.ctx.lineCap = 'round';
+            self.ctx.arc(self.circle_radius, self.circle_radius, (self.circle_radius - (self.line_width_progress / 2)), 1.5 * Math.PI, (-1.5 + (self.progress_start * 0.02) * Math.PI));
+            self.ctx.stroke();
+
+            self.progress_start++;
+            self.percent_count++;
+
+            self.progress_counter.text(self.percent_count);
+
+            if (self.progress_start >= self.percent) {
+                clearInterval(self.timer);
+            }
+        }, this.timer_rate);
+    }
+
+    drawStatic() {
+        // Static Progress Arc
+        this.ctx.beginPath();
+        this.ctx.strokeStyle = this.bar_color;
+        this.ctx.lineWidth = this.line_width_progress;
+        this.ctx.lineCap = 'round';
+        this.ctx.arc(this.circle_radius, this.circle_radius, (this.circle_radius - (this.line_width_progress / 2)), 1.5 * Math.PI, (1.5 + (this.percent * 0.02)) * Math.PI);
+        this.ctx.stroke();
+
+        this.progress_counter.text(this.percent);
     }
 }
 
@@ -103,7 +151,7 @@ function init_bst_circle_piechart() {
 
         var pieChartCircle = new PieChartCircle($this_piechart_circle, $percent, $size, $bar_color, $animated, $transition);
 
-        pieChartCircle.draw();
+        pieChartCircle.render();
     }
 }
 
